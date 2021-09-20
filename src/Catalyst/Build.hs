52d3939944d6983b9eadbce7cbb75d2f16b3cf1b
Chris Penner <christopher.penner@gmail.com>
2021-09-20 12:19:35 -0600

    Switch to STM-based env check

--- src/Catalyst/Build.hs
+++ src/Catalyst/Build.hs
@@ -18,20 +18,35 @@ import Control.Category.Product
 import Control.Category.Mon
 import Control.Monad.IO.Class
 import Data.Profunctor.Cayley
-import Data.IORef (newIORef, readIORef, writeIORef, IORef)
 import System.Posix.Files
 import Prelude hiding (readFile)
 import Control.Monad.State
 import Control.Concurrent
-import Control.Concurrent.STM.TVar
-import Control.Concurrent.STM
+import UnliftIO.STM
 import Data.Function
 import Data.Traversable.WithIndex
 import qualified Data.Map as Map
+import Data.Maybe
+import UnliftIO
 
 newtype Build (m :: * -> *) i o =
-    Build (IO (IO Status, i -> m o))
-    deriving (Category, Arrow, Profunctor, Strong, Closed) via (Cayley IO (CatProd (Mon (Tracker IO)) (Kleisli m)))
+    Build (IO (StatusTracker, i -> m o))
+    deriving (Category, Arrow, Profunctor, Strong, Closed) via (Cayley IO (CatProd (Mon StatusTracker) (Kleisli m)))
+
+-- StatusTracker always tries to run each STM block,
+-- but will only succeed if at least one contained block succeeds.
+-- This allows arrows to "clear" their own status variables when they succeed
+-- and prevents multiple redundant re-runs.
+newtype StatusTracker = ST { runStatusTracker :: STM () }
+instance Semigroup StatusTracker where
+  ST l <> ST r = ST $ do
+    l' <- optional l
+    r' <- optional r
+    if isNothing l' && isNothing r' then retrySTM
+                                    else pure ()
+
+instance Monoid StatusTracker where
+  mempty = ST retrySTM
 
 data LR = L | R
   deriving (Eq)
@@ -39,20 +54,20 @@ data LR = L | R
 -- The derived version is too greedy with regard to the Status check.
 instance MonadIO m => Choice (Build m) where
   left' (Build setup) = Build $ do
-      (envCheck, f) <- setup
-      dirRef <- newIORef L
+      (ST envCheck, f) <- setup
+      dirRef <- newTVarIO L
       let go = \case
                  Left a -> do
-                     liftIO $ writeIORef dirRef L
+                     liftIO $ atomically . writeTVar dirRef $ L
                      Left <$> f a
                  Right x -> do
-                     liftIO $ writeIORef dirRef R
+                     atomically $ writeTVar dirRef R
                      pure $ Right x
       let newCheck = do
-            readIORef dirRef >>= \case
+            readTVar dirRef >>= \case
               L -> envCheck
-              R -> pure Clean
-      pure $ (newCheck, go)
+              R -> retrySTM
+      pure $ (ST newCheck, go)
 
 instance MonadIO m => ArrowChoice (Build m) where
   left = left'
@@ -60,44 +75,34 @@ instance MonadIO m => ArrowChoice (Build m) where
 -- The instance derived via Cayley only runs setup once, whereas we need to run it for each value
 itraverse' :: (Show i, Ord i, MonadIO m, TraversableWithIndex i t) => Build m a b -> Build m (t a) (t b)
 itraverse' (Build setup) = Build $ do
-    stashRef <- newIORef Map.empty
-    let envCheck = do
-          readIORef stashRef >>= foldMap fst
+    stashRef <- newTVarIO Map.empty
+    let envCheck = ST $ do
+          readTVar stashRef >>= runStatusTracker . foldMap fst
     pure . (envCheck,) $ \xs -> do
-        funcMap <- liftIO $ readIORef stashRef
+        funcMap <- readTVarIO stashRef
         (os, resultMap) <- flip runStateT Map.empty . ifor xs $ \i x -> do
             Map.lookup i funcMap & \case
               Nothing -> liftIO setup >>= \r@(_, f) -> modify (Map.insert i r) *> liftIO (putStrLn $ "initializing: " <> show i) *> lift (f x)
               Just r@(_, f) -> modify (Map.insert i r) *> lift (f x)
-        liftIO $ writeIORef stashRef resultMap
+        atomically $ writeTVar stashRef resultMap
         pure os
 
-newtype Tracker checkM =
-    Tracker (checkM Status)
-
-instance Applicative checkM => Semigroup (Tracker checkM) where
-  Tracker l <> Tracker r = Tracker $ liftA2 (<>) l r
-
-instance Applicative checkM => Monoid (Tracker checkM) where
-  mempty = Tracker $ pure Clean
-
 build :: MonadIO m => m i -> (o -> m r) -> Build m i o -> m r
-build readInput handle (Build setup) = do
+build readInput handler (Build setup) = do
     (_, f) <- liftIO setup
     i <- readInput
     o <- f i
-    handle o
+    handler o
 
-watch :: (MonadIO m) => m i -> (o -> m r) -> Build m i o -> m a
-watch readInput handle (Build setup) = do
-    (envCheck, f) <- liftIO setup
-    void (readInput >>= f >>= handle)
-    let looper = forever $ do
+-- TODO: properly accept input OR check env changes
+watch :: (MonadIO m) => STM i -> (o -> m r) -> Build m i o -> m a
+watch readInput handler (Build setup) = do
+    (ST envCheck, f) <- liftIO setup
+    void (atomically readInput >>= f >>= handler)
+    forever $ do
           liftIO $ threadDelay 500000
-          liftIO envCheck >>= \case
-            Dirty -> void (readInput >>= f >>= handle)
-            Clean -> pure ()
-    looper
+          atomically (envCheck) -- wait for env changes
+          void (atomically readInput >>= f >>= handler)
 
 arrM :: (i -> m o) -> Build m i o
 arrM f = Build (pure (mempty, f))
@@ -110,30 +115,39 @@ tap = arrM $ \a -> liftIO (print a) *> pure a
 
 fileModified :: MonadIO m => Build m FilePath FilePath
 fileModified = Build $ do
+    (trigger, st) <- newTrigger
     (fpRef, tRef, go) <- trackInput 0 $ \fp -> do
         t <- modificationTime <$> liftIO (getFileStatus fp)
         pure (t, fp)
-    let check' :: IO Status
-        check' = do
-            liftIO (readIORef fpRef) >>= \case
-              Nothing -> pure Dirty
-              Just fp -> do
-                -- liftIO getWorkingDirectory >>= liftIO . print
-                lastT <- readIORef tRef
-                currentT <- modificationTime <$> liftIO (getFileStatus fp)
-                if lastT >= currentT
-                     then pure Clean
-                     else pure Dirty
-    pure $ (check', go)
+    void . forkIO . forever $ do
+        threadDelay 500000
+        fp <- atomically $ readTVar fpRef >>= maybe retrySTM pure
+        lastT <- readTVarIO tRef
+        currentT <- modificationTime <$> liftIO (getFileStatus fp)
+        if currentT > lastT then atomically (writeTVar tRef currentT >> trigger)
+                            else pure ()
+    pure $ (st, go)
 
-trackInput :: MonadIO m => s -> (i -> m (s, o)) -> IO (IORef (Maybe i), IORef s, i -> m o)
+newTrigger :: IO (STM (), StatusTracker)
+newTrigger = do
+    var <- newTVarIO False
+    pure (writeTVar var True, ST ((readTVar var >>= guard) <* writeTVar var False))
+
+waitForChange :: Eq a => a -> TVar a -> STM ()
+waitForChange a var = do
+    a' <- readTVar var
+    guard (a /= a')
+
+-- fileWatch :: 
+
+trackInput :: MonadIO m => s -> (i -> m (s, o)) -> IO (TVar (Maybe i), TVar s, i -> m o)
 trackInput def f = do
-    inpRef <- newIORef Nothing
-    stateRef <- newIORef def
+    inpRef <- newTVarIO Nothing
+    stateRef <- newTVarIO def
     pure $ (inpRef,stateRef,) $ \i -> do
-        liftIO $ writeIORef inpRef (Just i)
+        atomically $ writeTVar inpRef (Just i)
         (s, o) <- f i
-        liftIO $ writeIORef stateRef s
+        atomically $ writeTVar stateRef s
         pure o
 
 data Status = Dirty | Clean
@@ -154,20 +168,26 @@ eq a b = if a == b then Clean else Dirty
 
 cached' :: MonadIO m => (i -> i -> Status) -> Build m i o -> Build m i o
 cached' inputDiff (Build setup) = Build $ do
-    (checker, f) <- setup
-    lastRunRef <- newIORef Nothing
+    (ST checker, f) <- setup
+    (dirtify, ST checkDirty) <- newTrigger
+    lastRunRef <- newTVarIO Nothing
     let inner i = do
           let rerun = do o <- f i
-                         liftIO $ writeIORef lastRunRef (Just (i, o))
+                         atomically $ writeTVar lastRunRef (Just (i, o))
                          pure o
-          liftIO checker >>= \case
-            Dirty -> rerun
-            Clean -> do
-              liftIO (readIORef lastRunRef) >>= \case
+          atomically (optional checkDirty) >>= \case
+            Just _ -> rerun
+            Nothing -> do
+              readTVarIO lastRunRef >>= \case
                 Just (lastInput, o)
                   | inputDiff lastInput i == Clean -> pure o
                 _ -> rerun
-    pure (checker, inner)
+    pure (ST (checker *> dirtify), inner)
+
+waitJust :: TVar (Maybe a) -> STM a
+waitJust var = readTVar var >>= \case
+  Nothing -> retrySTM
+  Just a -> pure a
 
 ambient :: s -> StateT s IO Status -> IO (IO Status)
 ambient initial next = do
@@ -186,9 +206,9 @@ static (Build setup) i = Build $ do
 type Millis = Int
 debounced :: Applicative m => Millis -> Build m i i
 debounced millis = Build $ do
-    var <- newTVarIO Clean
-    void . forkIO . forever $ atomically (writeTVar var Dirty) *> threadDelay (millis * 1000)
-    pure $ (atomically (readTVar var <* writeTVar var Clean), pure)
+    (trigger, st) <- newTrigger
+    void . forkIO . forever $ atomically trigger *> threadDelay (millis * 1000)
+    pure $ (st, pure)
 
 example :: IO ()
 example = do
